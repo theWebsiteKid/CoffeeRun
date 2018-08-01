e505bd4df5f991d37b59b8ccf9f9e7ef82121fa9
Xavier Duncan <xduncan@pm.me>
2018-08-01 15:12:29 -0400

    practice merging git branches :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
+// init API url
+var url = 'https://dc-coffeerun.herokuapp.com/api/coffeeorders';
+// make GET request to server
+$.ajax(url,  {
+    method: 'GET',
+    success: function(data) {
+        console.log(data)
+    }
+});
 // init global variables
 var orders = [];
 var orderForm = document.querySelector('.orderForm');
 var container = document.querySelector('.orderList');
+var listItem = document.createElement('li');
 // save to localStorage
 var saveOrder = function() {
     localStorage.setItem('coffeeOrder', JSON.stringify(orders));
@@ -12,30 +22,43 @@ var loadOrder = function () {
     if (orders === null) {
         orders = [];
     }
-    console.log(orders);
 };
-// load orders on page load
-loadOrder(orders);
-console.log("2" + orders);
-// first function builds DOM elements on load
-order.forEach( function (element) {
+// store form values in new order object
+var getFormValues = function () {
     // init local variables
-    var order = document.querySelector('[name="order"]').value;
-    var email = document.querySelector('[name="email"]').value;
-    var size = document.querySelector('[name="size"]:checked').value;
-    var rating = document.querySelector('[name="rating"]').value;
-    var pendingOrder = {"order": order, "email": email, "size": size, "rating": rating};
+    var coffee = document.querySelector('[name="coffee"]');
+    var email = document.querySelector('[name="email"]');
+    var size = document.querySelector('[name="size"]');
+    var strength = document.querySelector('[name="strength"]');
+    // build new order object
+    var newOrder = {
+        "coffee": coffee,
+        "email": emailAddress,
+        "size": size,
+        "strength": strength
+    };
+    // return new order object
+    // now other functions can use it!
+    return newOrder;
+};
+// build DOM on page load
+orders.forEach(function () {
+    // load existing orders on page load
+    loadOrder(orders);
     // append to unordered list
-    var listItem = document.createElement('li')
-    listItem.textContent = `${order}, ${email}, ${size}, ${rating}`;
+    listItem.textContent = `
+        ${coffee}, ${email}, ${size}, ${strength}
+    `;
+    container.appendChild(listItem);
 });
-// Push form input to orders list
+// push form input to orders list
 // and persist to localStorage
 orderForm.addEventListener('submit', function (event) {
-    // prevent devault form submit
+    // prevent default form submit
     event.preventDefault();
-    container.appendChild(listItem);
-    orders.push(pendingOrder);
+    // push new order to orders array
+    orders.push(newOrder);
+    // persist to localStorage
     saveOrder(orders);
     // test!
     console.log(orders);
